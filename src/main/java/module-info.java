ff52f939569861eceebc82ceda76a726aa7ca697
Rodioo <antonioalex459@gmail.com>
2022-03-26 11:10:21 +0200

    Initialized project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module pao.proiectpao {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens pao.proiectpao to javafx.fxml;
+    exports pao.proiectpao;
+}
